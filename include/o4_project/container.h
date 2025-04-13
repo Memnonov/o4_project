f2a4b60a5c30dcb148007381593df1ee650ac527
mikko <mmnonov@gmail.com>
2025-04-13 19:24:01 +0300

    container has functions

--- include/o4_project/container.h
+++ include/o4_project/container.h
@@ -6,21 +6,24 @@
 #include <QVector>
 #include <item.h>
 #include <memory>
+#include <utility>
 
 class Container {
  public:
   QString name{"Unnamed Container"};
 
-  explicit Container(const QString name) : name{name} {}
+  explicit Container(const QString& name) : name{name} {}
 
-  Container(const QString name, const QVector<std::unique_ptr<Item>> items)
-      : name{name}, items{items} {}
+  Container(const QString& name, const QVector<std::unique_ptr<Item>> items)
+      : name{name}, items{std::move(items)} {}
 
   Container() = default;
   Container(const Container& other) = delete;
   Container& operator=(const Container& other) = delete;
 
-  void addItem();
+  void addItem(std::unique_ptr<Item> item);
+  void removeItem(unsigned int index);
+  void moveItem(unsigned int index, Container& other);
 
  private:
   QVector<std::unique_ptr<Item>> items{};
--- src/container.cpp
+++ src/container.cpp
@@ -1,7 +1,26 @@
 // Copyright [2025] Auli Jussila & Mikko Memonen
 
 #include "../include/o4_project/container.h"
+#include "../include/o4_project/item.h"
 #include <QtLogging>
+#include <memory>
 #include <qdebug.h>
+#include <utility>
 
-void Container::addItem() { qInfo() << "Adding an item!\n"; }
+void Container::addItem(std::unique_ptr<Item> item) {
+  if (item) {
+    items.push_back(std::move(item));
+  }
+}
+
+void Container::removeItem(unsigned int index) {
+  if (index < items.size()) {
+    items.removeAt(index);
+  }
+}
+
+void Container::moveItem(unsigned int index, Container& other) {
+  if (index < items.size()) {
+    other.addItem(std::move(items.takeAt(index)));
+  }
+}
