ef036c6dea8534bd2e7d7f68d7ad5787a1ee7c94
mikko <mmnonov@gmail.com>
2025-04-24 14:49:16 +0300

    move mode has some basic logic

--- include/o4_project/move_window.h
+++ include/o4_project/move_window.h
@@ -3,18 +3,34 @@
 #ifndef MOVE_WINDOW_H
 #define MOVE_WINDOW_H
 
+#include "container_window.h"
+#include "items_window.h"
 #include "mode_frame.h"
+#include <QStackedWidget>
 
 class MoveWindow : public ModeFrame {
- public:
-  MoveWindow(QWidget *parent = nullptr); 
+public:
+  MoveWindow(QWidget *parent = nullptr);
   ~MoveWindow() {}
 
- private:
-  QLabel *placeholder;
+private:
+  enum class Stack { right, left };
+
+  QPushButton *moveSelectedButton;
+  QStackedWidget *leftStack;
+  QStackedWidget *rightStack;
+  ContainerWindow *leftContainer;
+  ContainerWindow *rightContainer;
+  ItemsWindow *leftItems;
+  ItemsWindow *rightItems;
 
   // Makes this concrete.
   virtual void dummyFunction() {}
+  void initConnections();
+
+private slots:
+  void handleContainerSelected(Stack stack);
+  void handleContainerClosed(Stack stack);
 };
 
 #endif // !MOVE_WINDOW_H
--- src/move_window.cpp
+++ src/move_window.cpp
@@ -1,7 +1,63 @@
 #include "../include/o4_project/move_window.h"
 
 MoveWindow::MoveWindow(QWidget *parent)
-    : ModeFrame{parent}, placeholder{new QLabel{"Move items here!"}} {
-  layout->setAlignment(Qt::AlignCenter);
-  layout->addWidget(placeholder);
+    : ModeFrame{parent}, leftStack{new QStackedWidget},
+      moveSelectedButton{new QPushButton}, rightStack{new QStackedWidget},
+      rightContainer{new ContainerWindow}, leftContainer{new ContainerWindow},
+      leftItems{new ItemsWindow}, rightItems{new ItemsWindow} {
+  leftStack->addWidget(leftContainer);
+  leftStack->addWidget(leftItems);
+  layout->addWidget(leftStack);
+
+  moveSelectedButton->setIcon(QIcon(":/icons/arrow-separate.svg"));
+  moveSelectedButton->setText("Move Selected");
+  layout->addWidget(moveSelectedButton);
+
+  rightStack->addWidget(rightContainer);
+  rightStack->addWidget(rightItems);
+  layout->addWidget(rightStack);
+
+  layout->setStretch(0, 1);
+  layout->setStretch(2, 1);
+  initConnections();
+}
+
+void MoveWindow::initConnections() {
+  using Stack = MoveWindow::Stack;
+  connect(leftContainer, &ContainerWindow::containerSelected, this,
+          [this]() { handleContainerSelected(Stack::left); });
+  connect(rightContainer, &ContainerWindow::containerSelected, this,
+          [this]() { handleContainerSelected(Stack::right); });
+  connect(leftItems, &ItemsWindow::itemsWindowClosed, this,
+          [this]() { handleContainerClosed(Stack::left); });
+  connect(rightItems, &ItemsWindow::itemsWindowClosed, this,
+          [this]() { handleContainerClosed(Stack::right); });
+}
+
+void MoveWindow::handleContainerSelected(Stack stack) {
+  using Stack = MoveWindow::Stack;
+  switch (stack) {
+  case Stack::left: {
+    leftStack->setCurrentWidget(leftItems);
+    break;
+  }
+  case Stack::right: {
+    rightStack->setCurrentWidget(rightItems);
+    break;
+  }
+  }
+}
+
+void MoveWindow::handleContainerClosed(Stack stack) {
+  using Stack = MoveWindow::Stack;
+  switch (stack) {
+  case Stack::left: {
+    leftStack->setCurrentWidget(leftContainer);
+    break;
+  }
+  case Stack::right: {
+    rightStack->setCurrentWidget(rightContainer);
+    break;
+  }
+  }
 }
