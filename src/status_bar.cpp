1aa7a8d7116cd98dd2bd3d1dd7b63cddddc6307a
mikko <mmnonov@gmail.com>
2025-05-04 21:15:02 +0300

    status bar now sligtly narrower

--- src/status_bar.cpp
+++ src/status_bar.cpp
@@ -2,12 +2,20 @@
 #include <qlogging.h>
 #include <qnamespace.h>
 #include <qpushbutton.h>
+#include <qsizepolicy.h>
 
 StatusBar::StatusBar(QWidget *parent)
     : QWidget(parent), statusMessage{new QLabel}, undoButton{new QPushButton},
       redoButton{new QPushButton} {
+  setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+  setContentsMargins(0, 0, 0, 0);
   auto layout = new QHBoxLayout{this};
+  
+  layout->setContentsMargins(0, 0, 0, 0);
   layout->setAlignment(Qt::AlignRight);
+  statusMessage->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Minimum);
+  undoButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Minimum);
+  redoButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Minimum);
   
   statusMessage->setText("<i>This here is gonna be a status bar</i>");
   undoButton->setIcon(QIcon(":/icons/undo.svg"));
