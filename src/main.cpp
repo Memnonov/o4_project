d5e6bb1519a73d403cb2fbb09e7c6003c094b5b1
mikko <mmnonov@gmail.com>
2025-05-06 02:25:59 +0300

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 // Copyright [2025] Auli Jussila & Mikko Memonen
 
 #include "../include/o4_project/main_window.h"
-#include "../include/o4_project/json_utils.h"
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+  // Testing testing...
   QApplication app(argc, argv);
   MainWindow mainWindow;
   mainWindow.show();
